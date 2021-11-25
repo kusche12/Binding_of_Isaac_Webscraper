4b00dc38f3302681c8a88dee22321bac54de34c3
kusche12 <kylerkusche@gmail.com>
2021-11-24 16:22:38 -0800

    setting up sql

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
+// TODO: Set up a TLS 2.0 connection to the database
+// or else you will keep crashing on attempted connections.
+
 // Run the app with `nodemon app.js` and navigate to /scraper
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql');
+const data = require('./secrets.json');
 const { main, deleteAllItems } = require('./scrapers.js');
 const port = 3000;
+
+// Create connection to database
+const db = mysql.createConnection({
+    host: data.sqlConfig.host,
+    user: data.sqlConfig.user,
+    password: data.sqlConfig.password,
+    database: data.sqlConfig.database
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log("MySQL Connected...");
+    }
+});
+
 
 const app = express();
 
--- scrapers.js
+++ scrapers.js
@@ -22,51 +22,52 @@ async function scrapeItemPage(url) {
 }
 
 // Scrape the individual item for more detail
-async function scrapeItem(itemNames) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+async function scrapeItem(itemNames, db) {
+    console.log(db);
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
 
-    for (let i = 0; i < itemNames.length; i++) {
-        if (itemNames[i]) {
-            console.log('Going to page ' + itemNames[i]);
-            await page.goto(BASE_URL + itemNames[i].replace(" ", "_"));
+    // for (let i = 0; i < itemNames.length; i++) {
+    //     if (itemNames[i]) {
+    //         console.log('Going to page ' + itemNames[i]);
+    //         await page.goto(BASE_URL + itemNames[i].replace(" ", "_"));
 
-            // Check to see if this item page exists. If not, then iterate next
-            let notAnItem = await page.evaluate(() => {
-                let el = document.querySelector('.noarticletext');
-                return el;
-            });
-            if (isEmpty(notAnItem)) break;
+    //         // Check to see if this item page exists. If not, then iterate next
+    //         let notAnItem = await page.evaluate(() => {
+    //             let el = document.querySelector('.noarticletext');
+    //             return el;
+    //         });
+    //         if (isEmpty(notAnItem)) break;
 
-            // Scrape and clean to check if the item is active or passive
-            const [el] = await page.$x('//*[@id="mw-content-text"]/div/p[3]');
-            const txt = await el.getProperty('textContent');
-            const isActive = await txt.jsonValue();
-            const isActiveClean = cleanText(isActive);
-            let isActiveBool = true;
-            if (isActiveClean.includes('passive')) isActiveBool = false;
+    //         // Scrape and clean to check if the item is active or passive
+    //         const [el] = await page.$x('//*[@id="mw-content-text"]/div/p[3]');
+    //         const txt = await el.getProperty('textContent');
+    //         const isActive = await txt.jsonValue();
+    //         const isActiveClean = cleanText(isActive);
+    //         let isActiveBool = true;
+    //         if (isActiveClean.includes('passive')) isActiveBool = false;
 
-            // Get and clean the item trivia
-            let triviaListArray = [];
-            const [el2] = await page.$x('//*[@id="mw-content-text"]/div/ul[5]');
+    //         // Get and clean the item trivia
+    //         let triviaListArray = [];
+    //         const [el2] = await page.$x('//*[@id="mw-content-text"]/div/ul[5]');
 
-            // If there is no list of trivia, then it is a paragraph. Grab that instead
-            if (el2 === undefined) {
-                const [el3] = await page.$x('//*[@id="mw-content-text"]/div[1]/p[4]');
-                const txt3 = await el3.getProperty('textContent');
-                const txt3JSON = await txt3.jsonValue();
-                const txt3Clean = cleanText(txt3JSON);
-                triviaListArray.push(txt3Clean);
-            } else {
-                const triviaList = await el2.$x("li");
-                for (let line of triviaList) {
-                    const lineTxt = await page.evaluate(el => el.innerHTML, line);
-                    const lineTxtClean = cleanText(lineTxt)
-                    triviaListArray.push(lineTxtClean);
-                }
-            }
-        }
-    }
+    //         // If there is no list of trivia, then it is a paragraph. Grab that instead
+    //         if (el2 === undefined) {
+    //             const [el3] = await page.$x('//*[@id="mw-content-text"]/div[1]/p[4]');
+    //             const txt3 = await el3.getProperty('textContent');
+    //             const txt3JSON = await txt3.jsonValue();
+    //             const txt3Clean = cleanText(txt3JSON);
+    //             triviaListArray.push(txt3Clean);
+    //         } else {
+    //             const triviaList = await el2.$x("li");
+    //             for (let line of triviaList) {
+    //                 const lineTxt = await page.evaluate(el => el.innerHTML, line);
+    //                 const lineTxtClean = cleanText(lineTxt)
+    //                 triviaListArray.push(lineTxtClean);
+    //             }
+    //         }
+    //     }
+    // }
 
     browser.close();
 }
@@ -75,10 +76,10 @@ async function deleteAllItems() {
     console.log('delete all items');
 }
 
-async function main() {
+async function main(db) {
     const itemNames = await scrapeItemPage(BASE_URL + 'Items');
     // await scrapeItem(itemNames);
-    await scrapeItem(['Box of Spiders']);
+    await scrapeItem(['Box of Spiders'], db);
     // await scrapeItem(['Ball of Bandages']);
 }
 
